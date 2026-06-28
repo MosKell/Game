9fb01cd7b2ba96a36c7f077355b39f425173f313
MosKell <fesber.ml@gmail.com>
2026-06-28 21:14:31 +0300

    Add files via upload

---
+++ Game Q&A.py
@@ -0,0 +1,26 @@
+name = input(" Question 1: What is your name? ")
+print(" Great to meet you " + name + " !Let's start the game ")
+color = input(" Question 2: What is your favourite color? ")
+print(" Great I love it too ")
+guess = input("Question 3: What is 5 + 5 ?")
+if guess == "10":
+    print(" Correct, you got it right")
+else:
+    print(" False, try again ")
+friends = ["Mostafa", "Kelly", "Ali"]
+for person in friends:
+    print(" Welcome to the game," + person + "!")
+countdown = 3
+while countdown > 0:
+      print(countdown)
+      countdown = countdown - 1
+print(" Blast Off" )
+number = 10
+numberquantity = 10
+print(number * numberquantity )
+number = input(" What is 10 * 10 ?")
+if guess == 100:
+   print(" Correct ")
+else:
+    print(" False, try again ")
+print(" Thanks for playing the game ")
